b6d05ac4d539b1f02fca08fb9bec754f0abcb33d
Galvan <jgalvan@aus1m-techops88.ad.ea.com>
2021-01-31 20:28:03 -0600

    adding create exercise form

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -4,7 +4,15 @@ export default class CreateExercise extends Component {
 
     //CreateExercise subclass constructor
     constructor(props) {
+
         super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onsSubmit = this.onsSubmit.bind(this);
+
         this.state = {
             username: '',
             description: '',
@@ -12,6 +20,13 @@ export default class CreateExercise extends Component {
             date: new Date(),
             users: []
         }
+    }
+
+    componentDidMount() {
+        this.setState({
+            users: ['testUser'],
+            username: 'testUser'
+        })
     }
 
     onChangeUsername(e) {
@@ -55,9 +70,42 @@ export default class CreateExercise extends Component {
     }
 
     render() {
+        //Exercise log form
         return (
             <div>
-                <p>Create Exercise comp</p>
+                <h3>Create New Exercise Log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <select ref="userInput"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}>
+                        {
+                            this.state.users.map(function(user) {
+                                return <option
+                                key={user}
+                                value={user}>
+                                {user}
+                                </option>
+                            })
+                        }
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input 
+                        type="text"
+                        className="form-control"
+                        value={this.state.duration}
+                        onChange={this.onChangeDuration}
+                        />
+                    </div>
+                    <div>
+                        
+                    </div>
+                </form>
             </div>
         );
     }
